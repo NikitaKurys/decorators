3a31e7dfa5574fa989b7f8710252e3c28b5e1c01
NikitaKurys <kurys.nik@mail.ru>
2021-10-28 20:41:56 +0300

    second commit

--- decorators p.2.py
+++ decorators p.2.py
@@ -3,6 +3,7 @@ import os
 
 file_name = 'logs_two.txt'
 
+
 def path_to_log(path):
     def replacement(old_function):
         def new_function(*args, **kwargs):
@@ -14,10 +15,10 @@ def path_to_log(path):
                         f'вызвана: {data}\n '
                         f'аргументы: {args, kwargs}\n '
                         f'результат: {result}\n')
-            file = os.path.abspath(path)
-            return print(f'ПУТЬ ДО ЛОГА: {file}')
+            return result
         return new_function
     return replacement
+
 
 @path_to_log(file_name)
 def summator(x, y):
@@ -29,5 +30,13 @@ def divider(z, c):
     return z / c
 
 
-summator(9, 10)
-divider(12, 3)
+three = summator(1, 2)
+five = summator(2, 3)
+result_summator = summator(three, five)
+
+four = divider(8, 2)
+two = divider(16, 8)
+result_divider = divider(four, two)
+
+print(f'summator result: {result_summator}\nsummator result type: {type(result_summator)}')
+print(f'divider result: {result_divider}\ndivider result type: {type(result_divider)}')
